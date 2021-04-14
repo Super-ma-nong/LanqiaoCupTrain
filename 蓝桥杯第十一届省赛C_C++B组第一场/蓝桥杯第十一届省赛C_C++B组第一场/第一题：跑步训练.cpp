b7d94a074f22392401581405c871a98e3192a6cd
SuperCoder1999 <331150871@qq.com>
2021-04-14 23:14:28 +0800

    1

--- 蓝桥杯第十一届省赛C_C++B组第一场/蓝桥杯第十一届省赛C_C++B组第一场/第一题：跑步训练.cpp
+++ 蓝桥杯第十一届省赛C_C++B组第一场/蓝桥杯第十一届省赛C_C++B组第一场/第一题：跑步训练.cpp
@@ -1,19 +1,27 @@
-#include<iostream>
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int n = 10000;
+//	int min = 0;
+//	while (n) {
+//		if (n -= 600)
+//			min++;
+//		else
+//			break;
+//		if (n += 300)
+//			min++;
+//		else
+//			break;
+//	}
+//	printf("%d", min);
+//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
 
-using namespace std;
-
+int a=3;
 int main() {
-	int n = 10000;
-	int min = 0;
-	while (n) {
-		if (n -= 600)
-			min++;
-		else
-			break;
-		if (n += 300)
-			min++;
-		else
-			break;
-	}
-	printf("%d", min);
+	int a = 1;
+	printf("%d", a);
 }
